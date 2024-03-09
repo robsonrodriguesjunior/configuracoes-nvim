5da505c3214aa5171200bbb741f5596ee211eb82
robson <robson.rodrigues477557@outlook.com>
2024-03-08 21:48:22 -0300

    Adicionado treesitter

--- init.lua
+++ init.lua
@@ -22,7 +22,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
@@ -31,6 +32,13 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable = true }
+})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
