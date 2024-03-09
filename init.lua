8fd9c625bb9c0630d9200f7bd702aa42c9a17743
robson <robson.rodrigues477557@outlook.com>
2024-03-08 21:25:49 -0300

    Adicionado telescope

--- init.lua
+++ init.lua
@@ -17,11 +17,18 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
