e7c17995725048bff254b62a3966ba8056dedd3e
robson <robson.rodrigues477557@outlook.com>
2024-03-10 20:52:44 -0300

    Adicionado neotree

--- init.lua
+++ init.lua
@@ -13,25 +13,12 @@ if not vim.loop.fs_stat(lazypath) then
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
-  })
-end
+  }) end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
@@ -39,7 +26,4 @@ config.setup({
   highlight = { enable = true },
   indent = { enable = true }
 })
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
 
