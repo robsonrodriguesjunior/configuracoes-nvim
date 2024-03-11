26b2574c4863064e8d07df102266179c1fd99e15
robson <robson.rodrigues477557@outlook.com>
2024-03-10 21:12:14 -0300

    Adicionado lualine

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -16,14 +10,6 @@ if not vim.loop.fs_stat(lazypath) then
   }) end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
-
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
 
