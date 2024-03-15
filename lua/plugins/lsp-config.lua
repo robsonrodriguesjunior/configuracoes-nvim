28dfa1f6bf91ab93fcb589b92485aaa187967bd2
robson <robson.rodrigues477557@outlook.com>
2024-03-14 23:21:47 -0300

    Instalado o mason e o lua_sl

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+     requires = {
+      'williamboman/mason.nvim'
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+    end
+  }
+}
+
